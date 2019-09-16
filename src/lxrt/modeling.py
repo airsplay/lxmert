07a86bce3cb4139d65531b108eae3546a58fc7e6
Santiago Castro <sacastro@umich.edu>
2019-09-15 21:40:58 -0400

    Remove unnecessary __future__ imports

--- src/lxrt/modeling.py
+++ src/lxrt/modeling.py
@@ -16,8 +16,6 @@
 # limitations under the License.
 """PyTorch LXRT model."""
 
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import copy
 import json
 import logging
