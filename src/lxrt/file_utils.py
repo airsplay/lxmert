10fe3b16bd1f628dd1722fd05e3d77a597f07706
Santiago Castro <sacastro@umich.edu>
2019-09-15 21:50:32 -0400

    Update file_utils.py

--- src/lxrt/file_utils.py
+++ src/lxrt/file_utils.py
@@ -3,7 +3,6 @@ Utilities for working with the local dataset cache.
 This file is adapted from the AllenNLP library at https://github.com/allenai/allennlp
 Copyright by the AllenNLP authors.
 """
-
 import json
 import logging
 import os
